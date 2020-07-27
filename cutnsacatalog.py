9dbd807c935348a03e4abee61d66bc8b045c902f
rfinn <rfinn>
2020-07-27 10:16:10 -0400

    making version with vmax = 29k

---
+++ cutnsacatalog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''
+GOAL:
+- read in NSA and gswlc catalogs
+- save version with cz < 20000
+
+INPUT:
+- nsa catalog
+- gswlc catalog
+
+OUTPUT:
+- cut nsa catalog
+- cut gswlc catalog
+
+'''
+
+import os
+from astropy.table import Table
+
+homedir = os.getenv("HOME")
+outdir = homedir+'/research/APPSS/tables/'
+cat = homedir+'/research/NSA/nsa_v1_0_1.fits'
+nsa = Table.read(cat)
+flag = nsa['Z']*3.e5 < 2.e4
+nsa = nsa[flag]
+nsa.write(outdir+'nsa_v1_0_1_vmax20k.fits',overwrite=True)
+
+
+cat = tabledir+'/gswlc-A2-sdssphot-corrected.fits'
+gsw = Table.read(cat)
+flag = gsw['Z']*3.e5 < 2.e4
+gsw = gsw[flag]
+gsw.write(outdir+'gswlc-A2-sdssphot-corrected-vmax20k.fits',overwrite=True)
